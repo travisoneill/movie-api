8d5d171c357da2649785c8c035e52b22da5c6cad
travis.h.oneill@gmail.com <travis.h.oneill@gmail.com>
2017-02-10 16:03:48 -0800

    add query building method to movie model

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,7 @@
 class MoviesController < ApplicationController
 
   def show
+    byebug
     @movie = Movie.find(params[:id])
     if params[:relation] == 'related'
       render json: @movie.related_movies.map { |rel| rel.json_format(request.base_url, collection=true) }
@@ -11,6 +12,7 @@ class MoviesController < ApplicationController
 
 
   def index
+    byebug
     @movies = Movie.all
     render json: @movies.map { |movie| movie.json_format(request.base_url, collection=true) }
   end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,14 +2,6 @@ class Movie < ApplicationRecord
   has_many :movie_relationships, foreign_key: :movie_id1, class_name: 'MovieRelationship'
   has_many :related_movies, through: :movie_relationships, source: :other_movie
 
-
-
-  def attribute_format(attribute)
-    json_response = {}
-    json_response[:links] = {self: url + "/movies/#{self.id}"}
-
-  end
-
   def json_format(url, collection=false)
     json_response = {}
     json_response[:links] = {self: url + "/movies/#{self.id}"}
@@ -30,6 +22,39 @@ class Movie < ApplicationRecord
       }
     end
     return json_response
+  end
+
+  def build_query(request_params)
+    query_object = {}
+    order_object = {}
+    sort_order = :asc
+    case
+      when request_params[:title]
+        query_object[:title] = request_params[:title]
+      when request_params[:year]
+        years = request_params[:year].split('-')
+        start_year = years[0]
+        end_year = years[1] || start_year
+        query_object[:year] = [start_year, end_year]
+      when request_params[:description]
+        query_object[:description] = request_params[:description]
+      when request_params[:sort]
+        sort_attr = request_params[:sort]
+        order = request_params[:sort_order].downcase
+        if order
+          if ['d', 'desc', 'descending'].include(order)
+            sort_order = :desc
+          elsif ['a', 'asc', 'ascending'].include(order)
+            sort_order = :asc
+          else
+            raise 'INVALID SORT ORDER'
+          end
+        end
+        order_object[:sort_attr] = sort_order
+    end
+    puts query_object
+    puts order_object
+    return Movie.order(order_object).where(query_object)
   end
 
   private
