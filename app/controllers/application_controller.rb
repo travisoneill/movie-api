3e97ddda6023113709b40549ae9444854a5894c8
travis.h.oneill@gmail.com <travis.h.oneill@gmail.com>
2017-02-16 14:55:51 -0800

    error handling for unsupported params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::Base
 
   # send 415 if content type header does not match specification
   def check_headers
-    # byebug
     return if request.method == 'GET'
     if request.content_type != "application/vnd.api+json"
       render json: { errors: [ ErrorObject.new(415).object ] }, status: 415
@@ -13,31 +12,54 @@ class ApplicationController < ActionController::Base
   end
 
   def check_query_params
-    # byebug
-    allowed = PERMITTED[request.params[:controller].to_sym]
-    #TODO: FIX DEPRECATION ISSUE
+    allowed = PERMITTED[request.params[:controller].to_sym] + PERMITTED[:always]
+    params.each do |param|
+      unless allowed.include?(param.to_sym)
+        unpermitted_query
+        return
+      end
+    end
     if params[:sort]
       check_sort
     end
-    ActionController::Parameters.new(request.params).permit(*allowed)
   end
 
   def check_sort
-    sort_attrs = ['title', 'year', 'id']
     if params[:sort]
+      # byebug
+      allowed = SORT_ATTRS[:always] + SORT_ATTRS[request.params[:controller].to_sym]
       attribute = params[:sort][0] == '-' ? params[:sort][1..-1] : params[:sort]
-      unless sort_attrs.include?(attribute)
-        response = ErrorObject.new(404)
-        response.set_message("Unsupported Sort Parameter: #{params[:sort]}. Supported sort params: 'title', 'year', 'id'. Add a leading '-' to sort descending.")
-        response.set_link('fetching-sorting')
-        render json: { errors: [response] }, status: 404
+      unless allowed.include?(attribute)
+        unpermitted_sort
       end
     end
+  end
+
+
+  def unpermitted_sort
+    response = ErrorObject.new(404)
+    response.set_message("Unsupported Sort Parameter: #{params[:sort]}. Supported sort params: 'title', 'year', 'id'. Add a leading '-' to sort descending.")
+    response.set_link('fetching-sorting')
+    render json: { errors: [response] }, status: 404
+  end
+
+  def unpermitted_query
+    response = ErrorObject.new(404)
+    response.set_message("Unsupported Query Parameter for '#{request.params[:controller]}'")
+    response.set_link('query-parameters')
+    render json: { errors: [response] }, status: 404
   end
 
 end
 
 #add new permitted params to an array with the key set to the controller name
 PERMITTED = {
+  'always': [:controller, :action],
   'movies': [:title, :year, :sort, :id]
+}
+
+#add new permitted sort attributes here
+SORT_ATTRS = {
+  'always': [],
+  'movies': ['title', 'year', 'id']
 }
